e4233b18d0235d9b558ef3549ad65bdbb0b687b5
wutong <tong.wu@netviewtech.com>
2019-10-25 10:24:05 +0800

    Init

---
+++ tomFFRTMP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tomFFRTMP
+//
+//  Created by codew on 10/25/19.
+//  Copyright © 2019 itmoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tomFFRTMP/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  tomFFRTMP
+//
+//  Created by codew on 10/25/19.
+//  Copyright © 2019 itmoy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
